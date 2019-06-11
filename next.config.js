59532813e64e87b224a7ee0f060e517f35ffd893
tangjing <tangjing@zhihu.com>
2019-06-11 15:26:26 +0800

    fix: netlify

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const webpackConfig = require('./webpack.config')
 
 module.exports = withCss(
   withSass({
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/quarter' : '',
     webpack: config => {
       // add polyfill
       const originalEntry = config.entry
--- out/_next/static/chunks/styles.e1d0fb8e37b20374a3b0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([["ad9d"],{RDVA:function(n,o,c){},iec2:function(n,o,c){},kxKB:function(n,o,c){},tXfN:function(n,o,c){}}]);
