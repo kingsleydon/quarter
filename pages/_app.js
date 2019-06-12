0a6ad48d3278af04e94509d4fa67d8b45bcc4e19
tangjing <tangjing@zhihu.com>
2019-06-12 14:16:21 +0800

    fix: cnzz

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,22 @@ export default class NextApp extends App {
       <Container>
         <Head>
           <title>数媒网络毕业展邀请函</title>
-          <style>{'body > a {display: none}'}</style>
+          <meta
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+            name="viewport"
+          />
+          <meta name="full-screen" content="yes" />
+          <meta name="browsermode" content="application" />
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html:
+                "var cnzz_s_tag = document.createElement('script');cnzz_s_tag.type = 'text/javascript';cnzz_s_tag.async = true;cnzz_s_tag.charset = 'utf-8';cnzz_s_tag.src = 'https://w.cnzz.com/c.php?id=1277704529&async=1';var root_s = document.getElementsByTagName('script')[0];root_s.parentNode.insertBefore(cnzz_s_tag, root_s);",
+            }}
+          />
+          <style
+            dangerouslySetInnerHTML={{__html: 'body > a {display: none}'}}
+          />
         </Head>
         <Component />
       </Container>
